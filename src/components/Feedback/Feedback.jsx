b1dae1707b69db58dc251ae1b474068c39d1b45d
anastasiia <korn.anastasia02@gmail.com>
2025-03-01 20:02:21 +0200

    fixed feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,23 +3,23 @@ import css from './Feedback.module.css'
 import Notification from '../Notifications/Notification';
 
 const Feedback = ({ good, neutral, bad, totalFeedback, positivePercentage }) => {
-  let content;
-  if (totalFeedback > 0) {
-    content = (
-      <>
-        <h2 className={css.feedback_title}>Statistics</h2>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total feedback: {totalFeedback}</p>
-        <p>Positive feedback: {positivePercentage}%</p>
-      </>
-    );
-  } else {
-    content = <Notification />;
-  }
-
-  return <div>{content}</div>;
+  return (
+    <div>
+      <h2 className={css.feedback_title}>Statistics</h2>
+      {totalFeedback > 0 ? (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total feedback: {totalFeedback}</p>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </>
+      ) : (
+        <Notification message="No feedback given yet" />
+      )}
+    </div>
+  );
 };
+
 
 export default Feedback;
